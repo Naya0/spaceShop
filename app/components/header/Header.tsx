32392a9c41d33983ec5af63b05531aea3f4b0bfb
Anastasia <naya.st.a@yandex.ru>
2025-08-11 20:57:03 +0700

    Прикреплена шапка при скроле

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router";
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      setHasScrolled(scrollTop > 60);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="w-5/6 h-[60px] m-auto flex justify-between items-center bg-white">
-      <nav className="text-xs hover:text-gray-600 flex-1">
-        <NavLink to="/" end>
-          Home
-        </NavLink>
-        {/* <NavLink to="/trending" end>
+    <header
+      className=" h-[60px] w-full flex items-center justify-center z-50 fixed  border-b border-white"
+      style={{ backgroundColor: hasScrolled ? "white" : "transparent"}}
+    >
+      <div className="w-5/6 flex justify-between items-center">
+        <nav className="text-xs hover:text-gray-600 flex-1" style={{ color: hasScrolled ? "#1c1c1c" : "white"}}>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          {/* <NavLink to="/trending" end>
           Trending Concerts
         </NavLink> */}
-      </nav>
-      <div className="text-xl uppercase text-gray-800 flex justify-center flex-1">
-        <NavLink to="/" end>
-          Stellar Dreams
-        </NavLink>
-      </div>
-      <div className="flex justify-end items-center gap-8 flex-1">
-        <div className="text-xs hover:text-gray-600">Корзина</div>
-        <div className="text-xs hover:text-gray-600">Понравившееся</div>
+        </nav>
+        <div className="text-xl uppercase flex justify-center flex-1" style={{ color: hasScrolled ? "#1c1c1c" : "white"}}>
+          <NavLink to="/" end>
+            Stellar Dreams
+          </NavLink>
+        </div>
+        <div className="flex justify-end items-center gap-8 flex-1" style={{ color: hasScrolled ? "#1c1c1c" : "white"}}>
+          <div className="text-xs hover:text-gray-600">Корзина</div>
+          <div className="text-xs hover:text-gray-600">Понравившееся</div>
+        </div>
       </div>
     </header>
   );
