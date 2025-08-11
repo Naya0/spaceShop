9a35cf93aabb1711c5c05f2821acfbdf939a8ac3
Anastasia <naya.st.a@yandex.ru>
2025-08-11 20:57:59 +0700

    добавлена анимация

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,9 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 import Header from "~/components/header/Header";
 import Banner from "~/components/homeComponents/Banner";
 import PopularProducts from "~/components/homeComponents/PopularProducts";
+import AnimationTextBlock from "~/components/homeComponents/AnimationTextBlock";
+import { useScroll, animated } from "@react-spring/web";
 
 // export function meta({}: Route.MetaArgs) {
 //   return [
@@ -12,12 +13,18 @@ import PopularProducts from "~/components/homeComponents/PopularProducts";
 // }
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+
   return (
     <>
       <Header />
-      <main>
+      <main className="scroll-smooth">
         <Banner />
         <PopularProducts />
+
+        <animated.div style={{ opacity: scrollYProgress }}>
+          <AnimationTextBlock />
+        </animated.div>
       </main>
     </>
   );
