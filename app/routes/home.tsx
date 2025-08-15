73c99440de045c734d0038350fa223c861ef4084
Anastasia <naya.st.a@yandex.ru>
2025-08-15 19:59:51 +0700

    Добавлено получение данных в home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,35 +1,57 @@
-import Header from "~/components/header/Header";
 import Banner from "~/components/homeComponents/Banner";
-import PopularProducts from "~/components/homeComponents/PopularProducts";
 import AnimationTextBlock from "~/components/homeComponents/AnimationTextBlock";
 import { useScroll, animated } from "@react-spring/web";
 import EasyOrder from "~/components/homeComponents/sectionEasyOrder/EasyOrder";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getCategories } from "~/features/categories/categoriesSlice";
 import { getProducts } from "~/features/products/productsSlice";
 import type { AppDispatch } from "~/features/store";
-
+import ProductsList from "~/components/products/ProductsList";
+import { useAppSelector } from "~/features/hooks";
+import CategoriesList from "~/components/products/CategoriesList";
 
 export default function Home() {
   const { scrollYProgress } = useScroll();
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    dispatch(getProducts())
-  }, [dispatch])
+    dispatch(getCategories());
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  const products = useAppSelector((state) => state.products);
+  const categories = useAppSelector((state) => state.categories);
 
   return (
     <>
-      {/* <Header /> */}
-      {/* <main className="scroll-smooth"> */}
-        <Banner />
-        <PopularProducts />
+      <Banner />
+        {products.isLoading ? (
+          <p className="text-center">Loading...</p>
+        ) : (
+          <>
+            <ProductsList
+              title="Популярные товары"
+              amount={3}
+              products={products.list}
+            />
 
-        <animated.div style={{ opacity: scrollYProgress }}>
-          <AnimationTextBlock />
-          <EasyOrder />
-        </animated.div>
-      {/* </main> */}
+            <animated.div style={{ opacity: scrollYProgress }}>
+              <AnimationTextBlock />
+              <EasyOrder />
+            </animated.div>
+          </>
+        )}
+
+        {!products.isLoading && categories.isLoading ? (
+          <p className="text-center">Loading...</p>
+        ) : (
+          <CategoriesList
+            title="Категории"
+            amount={2}
+            categories={categories.list}
+          />
+        )}
     </>
   );
 }
