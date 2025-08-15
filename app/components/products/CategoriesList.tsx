c161b14b7ec81c50e117af2291877fc1b9fc94c4
Anastasia <naya.st.a@yandex.ru>
2025-08-15 20:01:53 +0700

    Добавлены компоненты для работы с категориями

---
+++ app/components/products/CategoriesList.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import CategoryCart from "./CategoryCart";
+import type { Category } from "~/types/product.types";
+import { Link } from "react-router";
+
+interface CategoriesSectionProps {
+  title: string;
+  categories: Category[];
+  amount?: number;
+}
+
+const CategoriesList = ({
+  title,
+  categories,
+  amount = 2,
+}: CategoriesSectionProps) => {
+  const categoriesList = categories.filter((_, i) => i < amount);
+
+  return (
+    <section>
+      <h2>{title}</h2>
+      <div
+        className="grid gap-5"
+        style={{ gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))" }}
+      >
+        {categoriesList.map((category) => (
+          <Link to={`/categories/${category.id}`}  key={category.id} >
+            <CategoryCart category={category} />
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CategoriesList;
---
+++ app/routes/categoryPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router";
+
+
+const categoryPage = () => {
+  const { id } = useParams<{id: string }>();
+  return <div>{id}</div>;
+};
+
+export default categoryPage;
