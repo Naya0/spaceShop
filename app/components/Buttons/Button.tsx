e85c15176d50b87378c8cf0b1a483c9ed197033f
Anastasia <naya.st.a@yandex.ru>
2025-08-15 19:59:08 +0700

    Адаптированы стили кнопок

--- app/components/Buttons/Button.tsx
+++ app/components/Buttons/Button.tsx
@@ -1,24 +1,32 @@
 import React from "react";
+import { Link } from "react-router";
 
 interface Button {
   height?: number;
   width?: number;
-  theme: string;
+  theme: "dark" | "light";
   link: string;
   title: string;
 }
 
 const Button = ({ height = 50, width = 250, theme, link, title }: Button) => {
   return (
+    <Link to={'/catalog'}>
     <div
-      className="bg-white relative flex justify-center items-center
-      hover:bg-[#9b9b9b42] hover:cursor-pointer hover:text-white
-      border-solid border border-white
-      transition delay-150 duration-300"
+      className={`
+    bg-white 
+      relative flex justify-center items-center
+      hover:cursor-pointer  hover:text-white
+      ${theme == "light" ? "hover:bg-[#9b9b9b42] " : "hover:bg-gray-700 hover:border-gray-700"}
+      border-solid border
+      ${theme == "light" ? "border-white" : "border-gray-600"}
+      transition delay-150 duration-300
+      `}
       style={{ width: width + "px", height: height + "px" }}
     >
       {title}
     </div>
+    </Link>
   );
 };
 
--- app/components/Buttons/ButtonsIcon.tsx
+++ app/components/Buttons/ButtonsIcon.tsx
@@ -6,6 +6,7 @@ interface Button {
   theme: string;
   link: string;
   icon: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 const ButtonsIcon = ({
@@ -14,17 +15,29 @@ const ButtonsIcon = ({
   theme,
   link,
   icon,
+  onClick,
 }: Button) => {
   return (
-    <div
-      className="bg-transparent relative flex justify-center items-center
-      hover:bg-[#c4c4c442] hover:cursor-pointer hover:text-white
-      border-solid border border-gray-300
-      transition delay-150 duration-300"
+    <button
+      className={`
+      bg-white 
+      relative flex justify-center items-center
+      hover:cursor-pointer  hover:text-white
+      ${theme == "light" ? "hover:bg-[#9b9b9b42] " : "hover:bg-gray-300 hover:border-gray-300"}
+      border-solid border
+      ${theme == "light" ? "border-white" : "border-gray-600"}
+      transition delay-150 duration-300      
+      `}
       style={{ width: width + "px", height: height + "px" }}
+      onClick={onClick}
     >
-     <img src={`/images/` + icon } alt="изображение нопки" className="w-1/2 h-1/2 brightness-70" aria-hidden='true'/>
-    </div>
+      <img
+        src={`/images/` + icon}
+        alt="изображение нопки"
+        className="h-1/2 brightness-70 aspect-square "
+        aria-hidden="true"
+      />
+    </button>
   );
 };
 
