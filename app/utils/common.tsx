e9822ebae4820345845d6a874d6f29a96747fc90
Anastasia <naya.st.a@yandex.ru>
2025-08-19 18:26:12 +0700

    Добавлен файл с общими функциями

---
+++ app/utils/common.tsx
@@ -0,0 +1,7 @@
+export const sumBy = (arr: number[]) => {
+  return arr.reduce((prev, cur) => prev + cur, 0);
+};
+
+export const totalQuantity = (arr: number[]) => {
+  return arr.reduce((prev, cur) => prev + cur, 0);
+};
