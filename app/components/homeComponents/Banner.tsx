b8769a7aa9e67855f53ee88cd41afd86344f8575
Anastasia <naya.st.a@yandex.ru>
2025-08-19 18:27:36 +0700

    Добавлен адаптив блока

--- app/components/homeComponents/Banner.tsx
+++ app/components/homeComponents/Banner.tsx
@@ -11,7 +11,7 @@ const Banner = () => {
     >
       <div className="w-full h-full bg-[#08080875] p-8 ">
         <div className="w-5/6 mx-auto h-full flex flex-col justify-end gap-[16px] max-w-[1600px] m-auto">
-          <h1 className="text-4xl w-2/3 font-light text-amber-50">
+          <h1 className="text-4xl md:w-2/3 w-full font-light text-amber-50">
             Ручная космическая графика – каждая открытка и плакат созданы с
             любовью к звёздам
           </h1>
